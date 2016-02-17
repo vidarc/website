9d951fd965d8641c661715a46994865ee60e7a30
Matthew Ailes <mattailes@gmail.com>
2016-02-17 03:54:35 -0500

    added some layouts

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+'use strict'
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('home');
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about');
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact');
 });
 
 module.exports = router;
