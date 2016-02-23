922ec583d7c9a2f8ab9c693d867052473c2e4591
vidarc - Matt Ailes <mattailes@gmail.com>
2016-02-22 21:09:12 -0500

    i love handlebars!

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,57 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   var context = {
-    nav1: 'class="active"',
-    nav2: '',
-    nav3: '',
-    nav4: '',
-    nav5: ''
+    home: 'class="active"',
+    portfolio: '',
+    resume: '',
+    about: '',
+    contact: ''
   };
   res.render('home', context);
 });
 
 router.get('/portfolio', function (req, res, next) {
-	res.render('portfolio');
+  var context = {
+    home: '',
+    portfolio: 'class="active"',
+    resume: '',
+    about: '',
+    contact: ''
+  };
+	res.render('portfolio', context);
 });
 
 router.get('/resume', function (req, res, next) {
-	res.render('resume');
+  var context = {
+    home: '',
+    portfolio: '',
+    resume: 'class="active"',
+    about: '',
+    contact: ''
+  };
+	res.render('resume', context);
 });
 
 router.get('/about', function (req, res, next) {
-  res.render('about');
+  var context = {
+    home: '',
+    portfolio: '',
+    resume: '',
+    about: 'class="active"',
+    contact: ''
+  };
+  res.render('about', context);
 });
 
 router.get('/contact', function (req, res, next) {
-  res.render('contact');
+  var context = {
+    home: '',
+    portfolio: '',
+    resume: '',
+    about: '',
+    contact: 'class="active"'
+  };
+  res.render('contact', context);
 });
 
 module.exports = router;
