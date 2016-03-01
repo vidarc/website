fff207cece14618eeb1adb7fb5283ad9d1f81f2d
vidarc - Matt Ailes <mattailes@gmail.com>
2016-03-01 01:34:43 -0500

    none of that worked

--- router/routes/home.js
+++ router/routes/home.js
@@ -3,36 +3,11 @@
 var express = require('express');
 var router = express.Router();
 
-// mongodb example for insertion
-var insert = function (db, callback) {
-  db.collection('blog').insertOne( {
-    "name" : {
-      "first" : "Matthew",
-      "last" : "Ailes"
-    },
-    "post" : "Hello, this is a test post",
-    "date": new Date()
-  }, function (err, result) {
-    assert.equal(err, null);
-    callback();
-  });
-};
-
 router.get('/', function (req, res, next) {
   var context = {
     home: ' class="active"'
   };
   res.render('home', context);
-});
-
-router.get('/test', function (req, res, next) {
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    insert(db, function() {
-      db.close();
-    });
-  });
-  res.send('Hopefully it worked!');
 });
 
 module.exports = router;
