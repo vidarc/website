d2b976982ae603ceea94ce4417a7df21250ec647
vidarc - Matt Ailes <mattailes@gmail.com>
2016-02-29 23:54:02 -0500

    dumb mistake

--- router/index.js
+++ router/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = function (app) {
+  app.use('/', require('./routes/home'));
   app.use('/about', require('./routes/about'));
   app.use('/contact', require('./routes/contact'));
-  app.use('/home', require('./routes/home'));
   app.use('/portfolio', require('./routes/portfolio'));
   app.use('/resume', require('./routes/resume'));
 };
