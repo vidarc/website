f5a0a89b1fb66fd226f4ec1dffb254ca5c9251f2
vidarc - Matt Ailes <mattailes@gmail.com>
2016-03-21 21:27:24 -0400

    quite a few style changes. chaning up resume format to be more templatey. add style later

--- router/routes/resume.js
+++ router/routes/resume.js
@@ -5,7 +5,35 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
   var context = {
-    resume: ' class="active"'
+    resume: ' class="active"',
+    content: {
+      name: 'Matthew Ailes',
+      address: '103 S Boulevard #2, Richmond, VA 23220',
+      phone: '804-836-4208',
+      email: 'mattailes@gmail.com',
+      college: [
+        { name: 'Oregon State University',
+          date: '2016',
+          degree: 'Bachelor of Science -- Computer Science',
+          gpa: '3.0'},
+        { name: 'Virginia Commonwealth University',
+          date: '2013',
+          degree: 'Bachelor of Arts -- Political Science'}
+      ],
+      skills: [
+        { name: 'HTML5 / CSS3',
+          level: '90'},
+        { name: 'JavaScript',
+          level: '75'}
+      ],
+      work: [
+        { employer: 'Domino\'s Pizza',
+          title: 'Delivery Driver',
+          joined: 'January 2007',
+          left: 'Current',
+          }
+      ]
+    }
   };
 	res.render('resume', context);
 });
