ae3bb20e352b5c08b0530729359d2715bb970777
Matthew Ailes <mattailes@gmail.com>
2016-11-24 17:57:48 -0500

    express server added to handle outputing index

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require('express')
+
+let app = express()
+
+app.use(express.static('build'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/build/index.html')
+})
+
+let server = app.listen(3000, function() {
+  let host = server.address().address
+  let port = server.address().port
+
+  console.log('The Express server is running at: ' + host + ':' + port)
+})
