b89a566b006b43cc1d56fecb1b959f5b683b2a5b
Matthew Ailes <mattailes@gmail.com>
2016-12-03 13:53:49 -0500

    support for login added. will customize further

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { Button } from 'semantic-ui-react'
 
 export default class Login extends Component {
   render() {
-    return <div>
-      hello
-    </div>
+    if (this.props.auth.loggedIn())
+      return <Button primary onClick={this.props.auth.logout.bind(this)} as={Link} to='/'>Logout</Button>
+    else
+      return <Button primary onClick={this.props.auth.login.bind(this)}>Login</Button>
   }
 }
