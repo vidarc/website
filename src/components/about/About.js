9ebe71f9c8d9bc347aafcbdcdc2735a58f2acd05
Matthew Ailes <mattailes@gmail.com>
2017-02-08 22:27:53 -0500

    will do flow later

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import {
-  Container
-} from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import './About.css'
 
 export default class About extends Component {
