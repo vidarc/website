91108599b3b3ae28738491b809b5f438d362639d
Matthew Ailes <mattailes@gmail.com>
2017-04-01 11:25:24 -0400

    add test and make some links on the projects page

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { Container, List } from 'semantic-ui-react'
+import LinkList from './LinkList'
+import metLogo from '../../images/the_met_logo.png'
 
 const Routes = (route) => (
   <Route path={route.path} render={props => (
@@ -7,15 +10,24 @@ const Routes = (route) => (
   )} />
 )
 
+const links = [
+  {
+    id: 1,
+    image: metLogo,
+    path: '/projects/art',
+    description: 'Art from the Met Museum'
+  }
+]
+
 const Projects = ({routes}) => (
-  <div>
-    <p>
-      <Link to='/projects/art'>Art from the Met Museum</Link>
-    </p>
+  <Container textAlign='center' fluid text>
+    <List celled horizontal relaxed>
+      {links.map((link) => <LinkList key={link.id} link={link} /> )}
+    </List>
     {routes.map((route, i) => (
       <Routes key={i} {...route} />
     ))}
-  </div>
+  </Container>
 )
 
 export default Projects
