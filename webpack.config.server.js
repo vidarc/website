9177d932baa0b2b1b5103aa3445ce57f21b2ef5f
Matthew Ailes <mattailes@gmail.com>
2017-04-07 23:06:44 -0400

    need css and style loader for server

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -35,6 +35,13 @@ module.exports = {
         }
       },
       {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ],
+      },
+      {
         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
         loader: 'url-loader',
         options: {
