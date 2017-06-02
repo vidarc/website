d6bd68074b6c557e9cee8f6df3d3d5d4d84afea0
Matthew Ailes <mattailes@gmail.com>
2017-06-01 20:18:57 -0400

    updated dependencies and add a serviceWorker for offline use

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
+import serviceWorker from './serviceWorker'
 import './style/main.css'
 import './style/semantic/semantic.min.css'
 
@@ -13,6 +14,7 @@ function render(Component) {
       <Component />
     </BrowserRouter>
   ), document.getElementById('root'))
+  serviceWorker()
 }
 
 render(App)
