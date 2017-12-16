2f6df861e15a6efb8a2a799a1a558f4ca566acf9
Matthew Ailes <mattailes@gmail.com>
2017-12-15 23:52:25 -0500

    flow-typed definitions added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-// @flow
+/* @flow */
 
 import React from 'react'
 import { hydrate } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import configureStore from './client/store/configureStore'
-import App from './client/components/App'
-import './client/style/main.css'
-import './client/style/semantic/semantic.min.css'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import configureStore from './client/store/configureStore'
+import App from './client/components/App'
+import './client/style/main.css'
+import './client/style/semantic/semantic.min.css'
 
 const client = new ApolloClient({
   link: new HttpLink(),
