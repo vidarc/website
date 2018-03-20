f3174eee0778bfb922579bdfb54e70b4e7b6f165
Matthew Ailes <mattailes@gmail.com>
2018-03-19 23:51:42 -0400

    make routes.config a little DRY and fix some eslinting

--- src/routes.config.js
+++ src/routes.config.js
@@ -6,35 +6,38 @@ import Loadable from 'react-loadable'
 const Loading = ({ pastDelay }) => {
   if (pastDelay) {
     return <div>Loading...</div>
-  } else {
-    return null
   }
+  return null
 }
 
-const loadableComponent = path =>
-  Loadable({
-    loader: () => import(path),
-    loading: Loading,
-    delay: 200,
-  })
+const loadableComponent = (opts: Object) =>
+  Loadable(
+    Object.assign(
+      {
+        loading: Loading,
+        delay: 200,
+      },
+      opts,
+    ),
+  )
 
 const routes = [
   {
     key: 0,
     path: '/',
-    component: loadableComponent('./client/modules/home'),
+    component: loadableComponent({ loader: () => import('./client/modules/home') }),
     exact: true,
   },
   {
     key: 1,
     path: '/todo',
-    component: loadableComponent('./client/modules/todo'),
+    component: loadableComponent({ loader: () => import('./client/modules/todo') }),
     exact: true,
   },
   {
     key: 2,
     path: '/starwars',
-    component: loadableComponent('./client/modules/starwars'),
+    component: loadableComponent({ loader: () => import('./client/modules/starwars') }),
     exact: true,
   },
 ]
