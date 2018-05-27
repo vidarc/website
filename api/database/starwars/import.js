bfb3bfc50b812dfc948d01e9731b6c1c2c2c592c
Matthew Ailes <mattailes@gmail.com>
2018-05-26 23:32:02 -0400

    big change to folder structure for workspaces, maybe lerna

---
+++ api/database/starwars/import.js
@@ -0,0 +1,33 @@
+const admin = require('firebase-admin')
+
+const serviceAccount = require('./../../keys/firebase.json')
+
+const films = require('./films.json')
+const people = require('./people.json')
+const planets = require('./planets.json')
+const species = require('./species.json')
+const starships = require('./starships.json')
+const vehicles = require('./vehicles.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const db = admin.firestore()
+
+const insert = (type, data) => {
+  const doc = db.collection(`starwars_${type}`)
+
+  data.forEach(object => doc.add(object))
+}
+
+insert('films', films)
+insert('people', people)
+insert('planets', planets)
+insert('species', species)
+insert('starships', starships)
+insert('vehicles', vehicles)
+
+module.exports = {
+  insert,
+}
---
+++ website/App.js
@@ -0,0 +1,10 @@
+// @flow
+
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+import routeHelper from './utils/routeHelper'
+import routes from './routes.config'
+
+const App = () => <Container>{routeHelper(routes)}</Container>
+
+export default App
