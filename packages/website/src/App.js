4e30a9df3f8730c3709dc4d572afde7b5e0a0baf
Matthew Ailes <mattailes@gmail.com>
2018-05-28 17:00:47 -0400

    add emotion and some basic styling

--- packages/website/src/App.js
+++ packages/website/src/App.js
@@ -1,9 +1,26 @@
 // @flow
 
 import React from 'react'
+import { css, injectGlobal } from 'emotion'
+
 import routeHelper from './utils/routeHelper'
 import routes from './routes.config'
 
-const App = () => <div>{routeHelper(routes)}</div>
+injectGlobal`
+  body {
+    font-size: 1em;
+    font-family: 'Fira Sans', sans-serif;
+    font-weight: 400;
+    text-align: center;
+  }
+`
+
+const wrapper = css`
+  text-align: left;
+  margin: 0 auto;
+  width: 1024px;
+`
+
+const App = () => <div className={wrapper}>{routeHelper(routes)}</div>
 
 export default App
