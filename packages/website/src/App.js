8b7eadf2b3fe1929c86cf2909841c4eb6e7a1338
Matthew Ailes <mattailes@gmail.com>
2018-05-28 22:51:09 -0400

    add normalize.css

--- packages/website/src/App.js
+++ packages/website/src/App.js
@@ -1,26 +1,31 @@
 // @flow
 
 import React from 'react'
-import { css, injectGlobal } from 'emotion'
+import { injectGlobal } from 'emotion'
+import 'normalize.css'
 
 import routeHelper from './utils/routeHelper'
 import routes from './routes.config'
 
 injectGlobal`
   body {
-    font-size: 1em;
     font-family: 'Fira Sans', sans-serif;
     font-weight: 400;
     text-align: center;
+
+    & div {
+      text-align: left;
+      margin: 0 auto;
+      width: 1024px;
+    }
+  }
+
+  button {
+    border-radius: 5px;
+    padding: 5px 10px;
   }
 `
 
-const wrapper = css`
-  text-align: left;
-  margin: 0 auto;
-  width: 1024px;
-`
-
-const App = () => <div className={wrapper}>{routeHelper(routes)}</div>
+const App = () => <div>{routeHelper(routes)}</div>
 
 export default App
