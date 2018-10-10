0ff462c41b904621000c4d6474e150362a85368e
Matthew Ailes <mattailes@gmail.com>
2018-10-10 00:27:53 -0400

    ✨ more addons and setup for storybook

--- packages/ui/.storybook/config.js
+++ packages/ui/.storybook/config.js
@@ -1,4 +1,15 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
+import { withOptions } from '@storybook/addon-options'
+import { themes } from '@storybook/components'
+
+addDecorator(
+  withOptions({
+    name: '@mattailes/ui',
+    sortStoriesByKind: true,
+    hierarchySeparator: /\/|\./,
+    hierarchyRootSeparator: /\|/
+  })
+)
 
 const req = require.context('../src/stories', true, /\.stories\.js$/)
 
--- packages/ui/src/stories/Button.stories.js
+++ packages/ui/src/stories/Button.stories.js
@@ -4,15 +4,27 @@ import * as React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { decorate } from '@storybook/addon-actions'
 
-import Button from '../components/Button'
+import { Button } from '../components/Button'
 
-storiesOf('Button', module).add(
-  'Default',
-  withInfo(`
-      A simple Button component
-      ~~~js
-      <Button>Click Here</Button>
-      ~~~
-    `)(() => <Button primary>I am a Button</Button>),
-)
+const pickNative = decorate([args => [args[0].nativeEvent]])
+
+storiesOf('Components|Button', module)
+  .addDecorator(withInfo)
+  .add(
+    'Primary',
+    () => (
+      <Button
+        primary
+        {...pickNative.actions('onClick', { clearOnStoryChange: true })}
+      >
+        I am a Button!
+      </Button>
+    ),
+    {
+      info: {
+        inline: true,
+      },
+    },
+  )
