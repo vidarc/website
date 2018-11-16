660d8ad78bd37b2a13f7538e5417294de1d58750
Matthew Ailes <mattailes@gmail.com>
2018-11-16 01:28:00 -0500

    💚 this might work

--- .travis/lernaVersion.sh
+++ .travis/lernaVersion.sh
@@ -3,10 +3,11 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git remote add with-credentials https://${GH_TOKEN}@github.com/vidarc/website.git
 }
 
 version() {
-  lerna version minor --yes --no-commit-hooks
+  lerna version minor --yes --no-commit-hooks --git-remote with-credentials
 }
 
 setup_git
