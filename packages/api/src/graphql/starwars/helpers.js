0676bfbad32ca042d64c9688522b9f38c7650a96
Matthew Ailes <mattailes@gmail.com>
2018-11-16 21:28:11 -0500

    🐛 adding the id to api response

--- packages/api/src/graphql/starwars/helpers.js
+++ packages/api/src/graphql/starwars/helpers.js
@@ -9,12 +9,25 @@ const get = async (url: string) => fetch(url)
   .then(res => res.json())
   .catch(err => logger.error(err))
 
-const getAll = async (type: string | number) => get(`https://swapi.co/api/${type}/`).then(response => response.results)
+async function getAll(type: string | number) {
+  return get(`https://swapi.co/api/${type}/`).then(response => response.results)
+}
 
-const getOne = async (type: string | number, index: string | number) => get(`https://swapi.co/api/${type}/${index}`)
+async function getOne(type: string | number, index: string | number) {
+  return get(`https://swapi.co/api/${type}/${index}`).then(response => ({
+    ...response,
+    ...{ id: index },
+  }))
+}
 
 const loader = new DataLoader((urls: Array<string>) => {
-  const promises = urls.map(url => fetch(url).then(response => response.json()))
+  const promises = urls.map((url) => {
+    const id = url.split('/').reverse()[1]
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(response => ({ ...response, ...{ id } }))
+  })
 
   return Promise.all(promises)
 })
