df689f7962569af5581b816526738b7af316a23a
Matthew Ailes <mattailes@gmail.com>
2018-12-04 21:31:23 -0500

    🎨 use babel macro for @emotion/styled

--- packages/website/src/modules/StarWarsApp/components/FlexContainer.tsx
+++ packages/website/src/modules/StarWarsApp/components/FlexContainer.tsx
@@ -1,9 +1,9 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
 
 interface Props {
   direction: 'column' | 'row'
 }
-const FlexContainer = styled('div')<Props>`
+const FlexContainer = styled.div<Props>`
   display: flex;
   flex-direction: ${props => (props.direction ? props.direction : 'row')};
 `
