7338cbc50efbd27a09b0fea56f0833fe94ed0ab0
Matthew Ailes <mattailes@gmail.com>
2018-12-08 21:51:34 -0500

    ✅ adding a test for coverage

---
+++ packages/website/src/modules/StarWarsApp/components/__tests__/PersonInfo.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Person } from '@mattailes/types'
+import { shallow } from 'enzyme'
+
+import PersonInfo from '../PersonInfo'
+
+describe('PersonInfo component', () => {
+  it('renders without crashing', () => {
+    const data: Person = {
+      id: 1,
+      homeworld: 'homeworld',
+      species: [],
+      films: [],
+      eye_color: 'eye',
+      gender: 'male',
+      height: 'height',
+      hair_color: 'hair',
+      starships: [],
+      birth_year: '1999',
+      name: 'name',
+      mass: 'mass',
+      vehicles: [],
+      skin_color: '',
+    }
+
+    expect(shallow(<PersonInfo {...data} />)).toBeTruthy()
+  })
+})
