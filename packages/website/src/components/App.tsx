ad831811e9fc2fb17dc4b9b981a07b1e42dac681
Matthew Ailes <mattailes@gmail.com>
2018-12-09 17:41:08 -0500

    💄 bringing in some better typography styles

--- packages/website/src/components/App.tsx
+++ packages/website/src/components/App.tsx
@@ -1,32 +1,13 @@
 import * as React from 'react'
 
-import { css, Global } from '@emotion/core'
 import styled from '@emotion/styled'
 import { Router } from '@reach/router'
 import 'sanitize.css'
 
+import GlobalStyles from './GlobalStyles'
 import Loading from './Loading'
 import { Navigation } from './Navigation'
 import NotFound from './NotFound'
-
-const globalStyles = css`
-  body {
-    font-family: 'Fira Sans', sans-serif;
-    font-weight: 400;
-    text-align: center;
-    box-sizing: border-box;
-
-    *,
-    *:before,
-    *:after {
-      box-sizing: inherit;
-    }
-
-    a {
-      text-decoration: none;
-    }
-  }
-`
 
 const StyledDiv = styled.div`
   text-align: left;
@@ -36,13 +17,13 @@ const StyledDiv = styled.div`
 
 const AsyncHome = React.lazy(() => import('../modules/Home/Home'))
 const AsyncStarWars = React.lazy(() =>
-  import('../modules/StarWarsApp/StarWarsApp'),
+  import('../modules/StarWarsApp/StarWarsApp')
 )
 const AsyncTodo = React.lazy(() => import('../modules/TodoApp/TodoApp'))
 
 const App: React.FunctionComponent = () => (
   <StyledDiv>
-    <Global styles={globalStyles} />
+    <GlobalStyles />
     <Navigation />
     <React.Suspense fallback={<Loading />}>
       <Router>
---
+++ packages/website/src/components/GlobalStyles.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+import { css, Global } from '@emotion/core'
+
+export default () => (
+  <Global
+    styles={css`
+      body {
+        box-sizing: border-box;
+
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+
+        a {
+          text-decoration: none;
+        }
+      }
+    `}
+  />
+)
