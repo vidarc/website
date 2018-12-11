1f047c17aed02df5a22f6d9a7f0c2003a2869b20
Matthew Ailes <mattailes@gmail.com>
2018-12-10 20:17:09 -0500

    ✨ lerna bump based on PR title

--- .travis/lernaVersion.sh
+++ .travis/lernaVersion.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
+VERSION=patch
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git remote add with-credentials https://${GITHUB_TOKEN}@github.com/vidarc/website.git
 }
 
+getVersionBump() {
+  if [[ $TRAVIS_COMMIT_MESSAGE == *'[patch]'* ]]; then
+    echo patch
+  elif [[ $TRAVIS_COMMIT_MESSAGE == *'[minor]'* ]]; then
+    echo minor
+  elif [[ $TRAVIS_COMMIT_MESSAGE == *'[major]'* ]]; then
+    echo major
+  fi
+}
+
 version() {
   git fetch with-credentials
   git checkout master
-  lerna version patch --yes --no-commit-hooks --git-remote with-credentials
+  lerna version $1 --yes --no-commit-hooks --git-remote with-credentials
 }
 
 setup_git
+VERSION=$(getVersionBump)
 version
