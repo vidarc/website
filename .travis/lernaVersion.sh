c99a357099089169a38a1c424e046d36cba5c88a
Travis CI <travis@travis-ci.org>
2018-12-10 22:28:19 -0500

    💚 modifying the lerna script

--- .travis/lernaVersion.sh
+++ .travis/lernaVersion.sh
@@ -1,30 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 VERSION=patch
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git remote add with-credentials https://${GITHUB_TOKEN}@github.com/vidarc/website.git
-}
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote add with-credentials https://${GITHUB_TOKEN}@github.com/vidarc/website.git
 
-getVersionBump() {
-  if [[ $TRAVIS_COMMIT_MESSAGE == *'[patch]'* ]]; then
-    echo patch
-  elif [[ $TRAVIS_COMMIT_MESSAGE == *'[minor]'* ]]; then
-    echo minor
-  elif [[ $TRAVIS_COMMIT_MESSAGE == *'[major]'* ]]; then
-    echo major
-  fi
-}
 
-version() {
-  git fetch with-credentials
-  git checkout master
-  lerna version $1 --yes --no-commit-hooks --git-remote with-credentials
-}
+if [[ $TRAVIS_COMMIT_MESSAGE == *'[patch]'* ]]; then
+  VERSION=patch
+elif [[ $TRAVIS_COMMIT_MESSAGE == *'[minor]'* ]]; then
+  VERSION=minor
+elif [[ $TRAVIS_COMMIT_MESSAGE == *'[major]'* ]]; then
+  echo major
+fi
 
-setup_git
-VERSION=$(getVersionBump)
 echo doing a $VERSION bump
-version
+
+git fetch with-credentials
+git checkout master
+lerna version $1 --yes --no-commit-hooks --git-remote with-credentials
