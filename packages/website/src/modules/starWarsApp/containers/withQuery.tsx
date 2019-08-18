00a26c790ca0bf16be2bc9675128179673a642dc
Matthew Ailes <mattailes@gmail.com>
2019-08-17 22:30:55 -0400

    ♻️ update to use useQuery hook

--- packages/website/src/modules/starWarsApp/containers/withQuery.tsx
+++ packages/website/src/modules/starWarsApp/containers/withQuery.tsx
@@ -1,31 +1,29 @@
 import * as React from 'react'
-import { Query } from 'react-apollo'
+import { useQuery } from 'react-apollo'
 
-export default (query, Component) => props => {
+const withQuery = (query, Component) => props => {
   const { '*': pathParam, defaultId } = props
 
   const variables = {
     id: +pathParam || defaultId,
   }
 
+  const { networkStatus, loading, error, data, refetch } = useQuery(query, { variables })
+
+  if (networkStatus === 4) return <p>refetching............</p>
+
+  if (loading) return <p>loading..........</p>
+
+  if (error) return <p>error.............</p>
+
+  const handleRefetch = () => refetch()
+
   return (
-    <Query query={query} variables={variables}>
-      {({ networkStatus, loading, error, data, refetch }) => {
-        if (networkStatus === 4) return <p>refetching............</p>
-
-        if (loading) return <p>loading..........</p>
-
-        if (error) return <p>error.............</p>
-
-        const handleRefetch = () => refetch()
-
-        return (
-          <div>
-            <Component {...data} />
-            <ma-button onClick={handleRefetch}>Refetch!</ma-button>
-          </div>
-        )
-      }}
-    </Query>
+    <div>
+      <Component {...data} />
+      <ma-button onClick={handleRefetch}>Refetch!</ma-button>
+    </div>
   )
 }
+
+export default withQuery
