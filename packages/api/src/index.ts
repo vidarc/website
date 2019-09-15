5f56f602476971f7ed6472aea622ac2280fb13fe
Matthew Ailes <mattailes@gmail.com>
2019-09-15 18:07:26 -0400

    🍻 trying out a cache that may or may not work

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,7 +3,8 @@ import graphql from './graphql'
 import weatherApi from './weather'
 
 const graphqlServer = graphql()
+const weatherCache: Map<string, { timestamp: Date; data: any }> = new Map()
 
 export const api = functions.https.onRequest(graphqlServer)
 
-export const weather = functions.https.onRequest(weatherApi)
+export const weather = functions.https.onRequest((req, res) => weatherApi(req, res, weatherCache))
--- packages/api/src/weather/index.ts
+++ packages/api/src/weather/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response, config } from 'firebase-functions'
 import fetch from 'cross-fetch'
+import { differenceInMinutes } from 'date-fns'
 
 const BASE_URL = 'https://api.openweathermap.org'
 const BASE_API_URL = '/data/2.5/'
@@ -19,13 +20,28 @@ function createRequest(type: string, city: string, zip: number): URL {
   return url
 }
 
-async function weatherApi({ query }: Request, res: Response) {
-  const { city, type = 'forecast', zip = 23220 } = query
+function isNotStale(timestamp: Date) {
+  return differenceInMinutes(timestamp, new Date()) >= 10
+}
 
-  const request = createRequest(type, city, zip)
-  const response = await fetch(request.href)
+async function weatherApi(
+  { query, originalUrl }: Request,
+  res: Response,
+  cache: Map<string, { timestamp: Date; data: any }>
+) {
+  if (cache.has(originalUrl) && isNotStale(cache.get(originalUrl).timestamp)) {
+    console.log('sending cached data')
+    res.send(cache.get(originalUrl).data)
+  } else {
+    const { city, type = 'forecast', zip = 23220 } = query
 
-  res.send(await response.json())
+    const request = createRequest(type, city, zip)
+    const response = await fetch(request.href)
+    const data = await response.json()
+    cache.set(originalUrl, { timestamp: new Date(), data })
+
+    res.send(data)
+  }
 }
 
 export default weatherApi
