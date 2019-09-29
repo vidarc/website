2fbadf9a981ba8bc25988b1cfa2e32d4341bfe62
Matthew Ailes <mattailes@gmail.com>
2019-09-29 18:14:44 -0400

    🐛 serialize the user object with toJSON function

--- packages/api/src/user/onCreate.ts
+++ packages/api/src/user/onCreate.ts
@@ -8,7 +8,7 @@ export default function onCreate(user: functions.auth.UserRecord) {
   const collection = firestore().collection('users')
 
   collection.doc(user.uid).set({
-    ...user,
+    ...user.toJSON(),
     role: 'user'
   })
 }
