69aaa5f9faf7929a9b429e1d61cf3df62e4bac79
Matthew Ailes <mattailes@gmail.com>
2019-12-12 00:13:12 -0500

    :sparkles: trying out stencil's react bindings

---
+++ packages/ui-react/src/components.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@mattailes/ui';
+
+import { defineCustomElements, applyPolyfills } from '@mattailes/ui/loader';
+
+applyPolyfills().then(() => { defineCustomElements(window); });
+export const MaButton = /*@__PURE__*/createReactComponent<JSX.MaButton, HTMLMaButtonElement>('ma-button');
+export const MaIcons = /*@__PURE__*/createReactComponent<JSX.MaIcons, HTMLMaIconsElement>('ma-icons');
--- packages/ui/stencil.config.ts
+++ packages/ui/stencil.config.ts
@@ -1,9 +1,18 @@
+/* eslint-disable import/prefer-default-export */
 import { Config } from '@stencil/core'
 import { sass } from '@stencil/sass'
+import { reactOutputTarget } from '@stencil/react-output-target'
 
 export const config: Config = {
   namespace: 'mattailes',
   globalStyle: 'src/global/app.scss',
   plugins: [sass()],
-  outputTargets: [{ type: 'dist' }, { type: 'www', serviceWorker: null }],
+  outputTargets: [
+    { type: 'dist' },
+    { type: 'www', serviceWorker: null },
+    reactOutputTarget({
+      componentCorePackage: '@mattailes/ui',
+      proxiesFile: '../ui-react/src/components.ts'
+    })
+  ]
 }
