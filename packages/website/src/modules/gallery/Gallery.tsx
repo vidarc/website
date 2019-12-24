64ef53fa23675f39c6bfb4e34abd32c4c7474b62
Matthew Ailes <mattailes@gmail.com>
2019-12-24 12:10:22 -0500

    :art: super simple display of images from storage

--- packages/website/src/modules/gallery/Gallery.tsx
+++ packages/website/src/modules/gallery/Gallery.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RouteComponentProps } from '@reach/router'
+import firebase from 'firebase/app'
 
 interface Props extends RouteComponentProps {}
 
-const Gallery: React.SFC<Props> = () => <div>The Gallery App</div>
+const Gallery: React.SFC<Props> = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    async function getImages() {
+      const ref = firebase.storage().ref('images')
+      const images = await ref.listAll()
+      const paths = images.items.map(item => item.getDownloadURL())
+      setItems(await Promise.all(paths))
+    }
+
+    getImages()
+  }, [])
+
+  return (
+    <div>
+      <h2>The Gallery App</h2>
+      {items.map(item => (
+        <img key={item} src={item} alt={item} />
+      ))}
+    </div>
+  )
+}
 
 export default Gallery
