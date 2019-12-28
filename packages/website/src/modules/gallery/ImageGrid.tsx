d6d0644e0067620aaf1b7234df7e78da3d81bb53
Matthew Ailes <mattailes@gmail.com>
2019-12-28 00:47:32 -0500

    :beers: very simple overlay of full sized image

--- packages/website/src/modules/gallery/ImageGrid.tsx
+++ packages/website/src/modules/gallery/ImageGrid.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 import { ImageState } from './gallery.types'
@@ -29,19 +29,52 @@ const Container = styled.div`
   }
 `
 
+const OverlayImage = styled.img`
+  z-index: 999;
+  position: absolute;
+  left: 0;
+`
+
 const Image = styled.img`
+  cursor: pointer;
   :hover {
     animation: ${keyframes.bounce} 1000ms ease infinite;
   }
 `
 
-const ImageGrid: React.FunctionComponent<Props> = ({ images }) => (
-  <Container>
-    {images.map(({ thumbPath }) => (
-      <Image key={thumbPath} src={thumbPath} alt="gallery item" />
-    ))}
-  </Container>
-)
+const ImageGrid: React.FunctionComponent<Props> = ({ images }) => {
+  const [overlay, setOverlay] = useState('')
+
+  const handleClick = (url: string) => setOverlay(url)
+
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key !== 'Escape') return
+
+      setOverlay('')
+    }
+
+    window.addEventListener('keydown', handleEscape)
+
+    return function cleanup() {
+      window.removeEventListener('keydown', handleEscape)
+    }
+  })
+
+  return (
+    <Container>
+      {overlay && <OverlayImage src={overlay} alt="fullsized gallery item" />}
+      {images.map(({ thumbPath, fullPath }) => (
+        <Image
+          key={thumbPath}
+          src={thumbPath}
+          alt="gallery item"
+          onClick={() => handleClick(fullPath)}
+        />
+      ))}
+    </Container>
+  )
+}
 
 ImageGrid.defaultProps = {
   images: []
