7962617971b358d516fe45c0db995fc5170062d9
Matthew Ailes <mattailes@gmail.com>
2020-06-26 20:43:15 -0400

    :art: adding a fancy glowing header

--- packages/website/src/modules/mashin/Mashin.tsx
+++ packages/website/src/modules/mashin/Mashin.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 import styled from '@emotion/styled'
 
+const Header = styled.h1`
+  text-align: center;
+  color: #ffffff;
+  background: #333333;
+  text-shadow: #fff 0px 0px 5px, #fff 0px 0px 10px, #fff 0px 0px 15px, #ff2d95 0px 0px 20px,
+    #ff2d95 0px 0px 30px, #ff2d95 0px 0px 40px, #ff2d95 0px 0px 50px, #ff2d95 0px 0px 75px;
+`
+
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -12,17 +20,20 @@ interface Props extends RouteComponentProps {}
 
 const Mashin: React.FC<Props> = () => {
   return (
-    <Container>
-      <iframe
-        title="mashin"
-        width="560"
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allow="accelerometer; autoplay; muted; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </Container>
+    <>
+      <Header>MASHIN</Header>
+      <Container>
+        <iframe
+          title="mashin"
+          width="560"
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ"
+          frameBorder="0"
+          allow="accelerometer; autoplay; muted; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </Container>
+    </>
   )
 }
 
