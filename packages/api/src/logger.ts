7497f809c86fc3b1013ad8d875b212a2d48b8dd7
Matthew Ailes <mattailes@gmail.com>
2021-01-19 00:03:20 -0500

    :loud_sound: adding pino for logging

--- packages/api/src/logger.ts
+++ packages/api/src/logger.ts
@@ -1,8 +1,27 @@
-/* eslint-disable no-console */
+import pino from 'pino'
 
-const logger = {
-  error: console.error,
-  log: console.log,
+const pinoSeverity = {
+  trace: 'DEBUG',
+  debug: 'DEBUG',
+  info: 'INFO',
+  warn: 'WARNING',
+  error: 'ERROR',
+  fatal: 'CRITICAL',
 }
+
+const logger = pino({
+  messageKey: 'message',
+  formatters: {
+    level(label, number) {
+      return {
+        severity: pinoSeverity[label] || pinoSeverity.info,
+        level: number,
+      }
+    },
+    log(message) {
+      return { message }
+    },
+  },
+})
 
 export default logger
